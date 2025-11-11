0cd86a8258cdf39140606b2fc8de9f17c700663a
antoniaaraceli <antoniaaraceli.tech@gmail.com>
2025-11-11 12:17:28 -0600

    Fundamentals about variables

---
+++ 01-fundamentals/script.js
@@ -0,0 +1,41 @@
+/* VALUES AND VARIABLES
+let js = 'amazing';
+console.log(2+3+4);
+
+console.log('Jonas');
+
+console.log(23);
+
+let firstName = "Jonas";
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let myCountry = 'Mexico';
+let myContinent = 'North America';
+let myPopulation = 126000000;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(myPopulation);
+*/
+
+/* DATA TYPES - JS has dynamic typing 
+// PRIMITIVE DATA TYPES
+let age = 26; // number
+let firstName = 'Antonia'; // string
+let isProgrammer = true; // boolean
+let hobbie; // undefined
+let nullValue = null; // null
+let symbol = Symbol('id'); // symbol, not used much
+let bigInt = 1234567890123456789012345678901234567890n; // bigInt, not used much
+
+console.log(typeof hobbie); */
+
+/* LET, CONST AND VAR */
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; // error
+// const job; // error
