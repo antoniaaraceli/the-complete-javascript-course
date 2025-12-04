016adce2f790ae10d79056fe6890e035f675314d
antoniaaraceli <antoniaaraceli.tech@gmail.com>
2025-12-04 09:04:15 -0600

    Challenge 4 and while loop

--- 02-fundamentals/script.js
+++ 02-fundamentals/script.js
@@ -258,4 +258,25 @@ for(let i = 0; i < antonia.length; i++){
     console.log(antonia[i], typeof antonia[i]);
 }
 
+
+/////////// WHILE LOOP ///////////
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`FOR - Lifting weights repetition ${rep} 🏋️‍♂️!`);
+}
+
+let rep = 1;
+while (rep <= 10){
+    console.log(`WHILE - Lifting weights repetition ${rep} 🏋️‍♂️!`);
+    rep++;
+}
 */
+
+// Excersice
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6) console.log('Loop is about to end...')
+}
+console.log('Finally ' + dice)
